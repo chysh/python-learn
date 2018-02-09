46dfd163b71914be3702eb13cb229299e36572b9
chysh <yushuo7013@163.com>
2018-02-09 16:49:17 +0800

    send mail in python

---
+++ email/email.py
@@ -0,0 +1,23 @@
+#!/etc/bin/python
+# -*- utf:utf-8 -*-
+
+from email.mime.text import MIMEText
+from email.header import Header
+import smtplib
+msg = MIMEText('浮生若梦，为欢几何', 'plain', 'utf-8')
+
+from_addr = input('From: ')
+password = input('Password: ')
+
+to_addr = input('To: ')
+smtp_server = input('SMTP server: ')
+
+msg['Subject'] = Header('问候', 'utf-8').encode()
+msg['from'] = from_addr
+msg['to'] = to_addr
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
---
+++ email/emailtextpic.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_add(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+from_addr = input('From: ')
+password = input('Password: ')
+to_addr = input('To: ')
+smtp_server = input('SMTP server: ')
+
+msg = MIMEMultipart('alternative')
+msg['Subject'] = Header('来自SMTP的问候...', 'utf-8').encode()
+msg['From'] = _format_add('Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_add('yushuo <%s>' % to_addr)
+
+msg.attach(MIMEText('hello', 'plain', 'utf-8'))
+msg.attach(MIMEText('<html><body><h1>Hello</h1>' + 
+    '<p><img src="cid:0"></p>' + 
+    '</body></html>', 'html', 'utf-8'))
+
+with open('./dogyear.jpg', 'rb') as f:
+    mime = MIMEBase('image', 'jpg', filename='dogyear.jpg')
+    mime.add_header('Content-Disposition', 'attachment', filename='dogyear.jpg')
+    mime.add_header('Content-ID', '<0>')
+    mime.add_header('X-Attachment-ID', '0')
+
+    mime.set_payload(f.read())
+    encoders.encode_base64(mime)
+    msg.attach(mime)
+
+server = smtplib.SMTP(smtp_server, 25)
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
